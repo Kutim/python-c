3d51dd5f7d6c38f8e4b33b19a96b556613c6dda7
kutim <1252900197@qq.com>
2017-11-27 17:52:43 +0800

    cython—— 修正 测试中 输出异常

--- UsingC/UsingC/test.py
+++ UsingC/UsingC/test.py
@@ -7,11 +7,11 @@ try:
     print("初始化 队列成功")
     print("=======================")
     i=5
-    print("插入一个元素 {}" % (i))
+    print("插入一个元素 %d" % (i))
     q.append(i)
-    print("弹出一个元素 {}" % q.pop())
+    print("弹出一个元素 %d" % q.pop())
     print("=======================")
 except Exception as e:
-    print("发生异常： {}" % e)
+    print("发生异常： %s" % e)
 finally:
     print("测试完成")
